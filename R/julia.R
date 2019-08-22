372bd129e83e6c8f5801ba4316184d271e109fa3
moodymudskipper <antoine.fabri@gmail.com>
2019-08-22 04:21:30 +0200

    julia operators

---
+++ R/julia.R
@@ -0,0 +1,24 @@
+reserved <-
+  c("if", "else", "repeat", "while", "function", "for", "in", "next", "break",
+    "TRUE", "FALSE", "NULL", "Inf", "NaN", "NA", "NA_integer_", "NA_real_",
+    "NA_complex_", "NA_character_")
+
+reserved_regex <- paste0("`(", paste0("(",reserved,")", collapse="|"),")`")
+
+`?julia>` <- function(..., fun = JuliaCall::julia_command){
+  if(!requireNamespace("JuliaCall"))
+    stop("Install package JuliaCall to use `?julia>` function")
+  args <- eval(substitute(alist(...)))
+  if(length(args) == 1 && is.character(args[[1]])){
+    code <- args[[1]]
+  } else {
+    args <- lapply(args, rlang::expr_deparse)
+    code <- paste(args, collapse = " ")
+    code <- gsub(reserved_regex, "\\1", code)
+    code <- gsub(":=", "=", code,fixed = TRUE)
+    code <- gsub("+ +", "\n", code,fixed = TRUE)
+  }
+  fun(code)
+}
+
+`?!julia>` <- function(...) `?julia>`(..., fun = JuliaCall::julia_eval)
