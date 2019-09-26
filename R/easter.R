a17f3689f29833267fa81eb6183b8d07f6dc61b0
moodymudskipper <antoine.fabri@gmail.com>
2019-09-26 03:02:24 +0200

    clean code
    look for last relevant seen function in the stack instead of looking at precise places
    
    replace dot by data to avoid confusion with local `.`

--- R/easter.R
+++ R/easter.R
@@ -7,10 +7,12 @@
 # iris2 %>% group_by(Species) %>% summarize(!!!?is.numeric := mean)
 # iris2 %>% group_by(Species) %>% summarize(nested = ?is.numeric := mean)
 
-easter_summarize_splice <- function(expr){
+
+
+qm_summarize_splice <- function(expr){
   expr <- substitute(expr)
-  . <- eval.parent(quote(.),4)
-  group_vars <- dplyr::group_vars(.)
+  data <- eval.parent(quote(.),4)
+  group_vars <- dplyr::group_vars(data)
 
 
   if (is.symbol(expr)) expr <- list(expr)
@@ -24,25 +26,25 @@ easter_summarize_splice <- function(expr){
 
   if(is.function(lhs) || inherits(lhs, "formula")){
     detect_fun <- rlang::as_function(lhs)
-    vars <- setdiff(names(.),group_vars)
-    vars <- vars[sapply(.[vars], detect_fun)]
+    vars <- setdiff(names(data),group_vars)
+    vars <- vars[sapply(data[vars], detect_fun)]
   } else {
     vars <- lhs
   }
-  res <- summarize_at(.,vars, apply_fun) %>%
+  res <- summarize_at(data,vars, apply_fun) %>%
     ungroup() %>%
     select(-one_of(group_vars))
   res
 }
 
-easter_summarize_nest <- function(expr){
+qm_summarize_nest <- function(expr){
   stop("calls to `?` in `summarize` must be spliced, use `!!!?`")
 }
 
-easter_mutate_splice <- function(expr){
+qm_mutate_splice <- function(expr){
   expr <- substitute(expr)
-  . <- eval.parent(quote(.),4)
-  group_vars <- dplyr::group_vars(.)
+  data <- eval.parent(quote(.),4)
+  group_vars <- dplyr::group_vars(data)
 
 
   if (is.symbol(expr)) expr <- list(expr)
@@ -56,51 +58,48 @@ easter_mutate_splice <- function(expr){
 
   if(is.function(lhs) || inherits(lhs, "formula")){
     detect_fun <- rlang::as_function(lhs)
-    vars <- setdiff(names(.),group_vars)
-    vars <- vars[sapply(.[vars], detect_fun)]
+    vars <- setdiff(names(data),group_vars)
+    vars <- vars[sapply(data[vars], detect_fun)]
   } else {
     vars <- lhs
   }
   #if(expr[[1]] != quote(`:=`)) stop("invalid expression, form must be `?detect_fun := apply_fun`")
 
-  res <- transmute_at(.,vars, apply_fun) %>%
+  res <- transmute_at(data,vars, apply_fun) %>%
     ungroup() %>%
     select(-one_of(group_vars))
   res
 }
 
-easter_mutate_nest <- function(expr){
+qm_mutate_nest <- function(expr){
   stop("calls to `?` in `summarize` must be spliced, use `!!!?`")
 }
 
-easter_select_gather <- function(fun) {
+qm_select_gather <- function(fun) {
   vars <- eval.parent(quote(tidyselect::peek_vars()))
-  . <- eval.parent(quote(.),4)
+  data <- eval.parent(quote(.),4)
   if(is.function(fun)){
-    vars_lgl <- sapply(vars, function(x) fun(.[[x]]))
+    vars_lgl <- sapply(vars, function(x) fun(data[[x]]))
   } else if (is.character(fun)){
-    vars_lgl <- fun %in% names(.)
+    vars_lgl <- fun %in% names(data)
   }
   which(vars_lgl)
 }
 
 get_fallback_qm <- function(){
-  if(length(sys.calls()) >= 6 &&
-     identical(sys.call(-5), quote(vars_select_eval(.vars, quos)))){
-    `?` <- easter_select_gather
-  } else if(length(sys.calls()) >= 10 && identical(sys.call(-9)[[1]], quote(summarize))){
-    `?` <- easter_summarize_splice
-  } else if(length(sys.calls()) >= 3 && identical(sys.call(-2)[[1]], quote(summarise_impl))){
-    `?` <- easter_summarize_nest
-  } else if(length(sys.calls()) >= 10 && identical(sys.call(-9)[[1]], quote(mutate))){
-    `?` <- easter_mutate_splice
-  } else if(length(sys.calls()) >= 3 && identical(sys.call(-2)[[1]], quote(mutate_impl))){
-    `?` <- easter_mutate_nest
-  } else if(length(sys.calls()) >= 10 && identical(sys.call(-9)[[1]], quote(transmute))){
-    `?` <- easter_mutate_splice
-  } else if(length(sys.calls()) >= 3 && identical(sys.call(-2)[[1]], quote(transmute_impl))){
-    `?` <- easter_mutate_nest
-  } else {
-    `?` <- utils::`?`
-  }
+  all_calls <- as.character(lapply(sys.calls(), `[[`, 1))
+  funs <- c("summarize","summarize", "mutate", "transmute", "select", "gather")
+  funs_lgl <- funs %in% rev(all_calls)
+  if(!any(funs_lgl)) return(utils::`?`)
+
+  # find in call stack the last of those functions that was called
+  fun <- funs[which.max(funs_lgl)]
+  switch(fun,
+         summarize=,
+         summarise = qm_summarize_splice,
+         transmute=,
+         mutate = qm_mutate_splice,
+         gather=,
+         select= qm_select_gather
+  )
 }
